ad0cf7d2f9b50b470e495cbd669eb69519c04513
joeseiden <joe.seiden214@gmail.com>
2017-02-08 12:11:41 -0800

    First complete

--- W3D3/skeleton/app/models/course.rb
+++ W3D3/skeleton/app/models/course.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  name          :string(255)
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Course < ActiveRecord::Base
+  has_many :enrollments,
+    class_name: "Enrollment",
+    foreign_key: :course_id,
+    primary_key: :id
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
 end
